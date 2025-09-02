465524657f551d3500b51f10831571b2f12277bc
angcyo <angcyo@126.com>
2025-09-02 09:17:35 +0800

    init

--- git_clone.bat
+++ git_clone.bat
@@ -2,18 +2,6 @@
 rem 设置当前控制台为UTF-8编码
 chcp 65001 >> nul
 
+git remote -v
 git fetch
 git rebase origin/main
-
-set "folder=rust_crates"
-
-if exist "%CD%\%folder%\" (
-    cd %folder%
-    echo 准备拉取仓库：%folder%
-    git fetch
-    git rebase origin/main
-    cd ..
-) else (
-    echo 准备克隆仓库：%folder%
-    git clone "git@github.com:angcyo/%folder%.git"
-)
